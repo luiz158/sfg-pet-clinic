353ae6fbea423b973da80d4645eafd435146f160
adrian <moreleadrian@wp.pl>
2018-11-10 23:00:09 +0100

    addedvisits, updated owner with pet relations. Closes #4

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pes;
+
+    public Set<Pet> getPes() {
+        return pes;
+    }
+
+    public void setPes(Set<Pet> pes) {
+        this.pes = pes;
+    }
 }
