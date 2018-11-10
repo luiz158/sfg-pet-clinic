41f407f8408871f2bce87c93d8b172e0b8a4aa09
adrian <moreleadrian@wp.pl>
2018-11-10 23:07:51 +0100

    Added contact infor properties to owner. Closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -7,13 +7,40 @@ import java.util.Set;
  */
 public class Owner extends Person {
 
-    private Set<Pet> pes;
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
 
-    public Set<Pet> getPes() {
-        return pes;
+    public String getAddress() {
+        return address;
     }
 
-    public void setPes(Set<Pet> pes) {
-        this.pes = pes;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
