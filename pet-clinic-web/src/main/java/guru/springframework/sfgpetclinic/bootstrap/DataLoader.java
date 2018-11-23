f87a75bb9f401d86c97ef896f18ab7976e14a0bf
Alan Byrne <abyrne133@gmail.com>
2018-11-23 15:49:37 +0000

    spring config services maps

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -17,10 +17,11 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader(){
-        this.ownerService = new OwnerServiceMap();
-        this.vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
+
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
