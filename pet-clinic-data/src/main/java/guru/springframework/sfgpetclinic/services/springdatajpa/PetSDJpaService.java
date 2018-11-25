a63f0eb7401d5b1b7e49314f3a17a3418f790215
adrian <adrian@adrian.com>
2018-11-25 17:30:49 +0100

    Create Spring Data JPA Vet Specialty Service, Closes #22. + Additional refactoring: Services names, missing @Profile and @Service annotations, missing final keywords for repository objects.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -3,13 +3,17 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.repositories.PetRepository;
 import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
-    private PetRepository petRepository;
+    private final PetRepository petRepository;
 
     public PetSDJpaService(PetRepository petRepository) {
         this.petRepository = petRepository;
