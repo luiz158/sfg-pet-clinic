4eb4059525884151357aaa8306e06fd2ac22ebf5
Alan Byrne <abyrne133@gmail.com>
2018-12-01 12:23:07 +0000

    bootstap pets to owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,12 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
-    private Set<Pet> pets;
+    private Set<Pet> pets = new HashSet<>();
     private String address;
     private String city;
     private String telephone;
