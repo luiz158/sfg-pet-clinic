a2ba5d37a6d1f56f8449407b8b069cf88a1b87fd
pazhosch <d.m.baranov@gmail.com>
2019-04-09 10:24:04 +0300

    initial commit

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +10,5 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
+
 }
