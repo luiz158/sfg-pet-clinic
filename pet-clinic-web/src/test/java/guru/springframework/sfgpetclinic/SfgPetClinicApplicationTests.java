9db705b47df17c89751ad2a38fd61bb2ca50e840
chad.kidd <chad.kidd@enernoc.com>
2019-07-24 11:29:09 -0700

    add index page

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,11 +1,16 @@
 package guru.springframework.sfgpetclinic;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SfgPetClinicApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//
-//}
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
